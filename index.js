2219659c93a67dcb5dacc963a6a6521504b8600b
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-13 11:14:08 -0300

    feat: facebook config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Bottr = require('bottr')
 var BottrApp = require('bottr-app')
 
 var bot = new Bottr.Bot()
-bot.use(new BottrApp())
+bot.use(new Bottr.FacebookMessengerClient())
 
 bot.on('message_received', function (message, session) {
     if(message.text.match(/Hoje tem treino?/)) {
