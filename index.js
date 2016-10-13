9930bb047b70809ac54e348ac8de193bbac425ad
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-13 11:59:05 -0300

    feat: add greetings message

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var Bottr = require('bottr')
 var bot = new Bottr.Bot()
 bot.use(new Bottr.FacebookMessengerClient())
 
+bot.hears(/Olá!/, function (message, session) {
+    session.send("Olá, no que posso ajudar?")
+})
+
 bot.hears(/Hoje tem treino?/, function (message, session) {
     session.send("Tem sim!")
 })
