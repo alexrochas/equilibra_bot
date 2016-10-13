47169a7a67ccc849fbb1b8a2a21ba2765083168e
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-13 10:54:50 -0300

    feat: first dummy answer

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ var BottrApp = require('bottr-app')
 var bot = new Bottr.Bot()
 bot.use(new BottrApp())
 
+bot.on('message_received', function (message, session) {
+    if(message.text.match(/Hoje tem treino?/)) {
+        session.send("Tem sim!")
+    } else {
+        session.send("Não entendi. =/")
+    }
+})
+
 bot.listen()
