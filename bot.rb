6d8452a321ef00cd9115ba07770d5bdcee93c38e
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-18 15:04:12 -0200

    test: update token

--- bot.rb
+++ bot.rb
@@ -3,6 +3,7 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
+puts 'Started?'
 Bot.on :message do |message|
   puts 'Message received'
   message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
@@ -20,7 +21,7 @@ Bot.on :message do |message|
   )
 end
 
-Facebook::Messenger.config.access_token = 'EAADkaiNsHSYBAFWTsjcQDNRi0JutyJtBXiknbesx6mdlPLiTKvpcxR9mBNMqjTtXPCEElCWR21cgblR88p3noWzseieTHddQZA5srK2y9c4sl3BQZAsaTZCEbGhFnm7yuLGMkmKNCFT8XvZCKNtu6jdTBwtkkovGC8ZAUDq3TaAZDZD'
+Facebook::Messenger.config.access_token = 'EAADkaiNsHSYBAJYOZCl91UUn9O41iiFFlpnoeDvqyUJvnxGVwZAhdNgGsB3VrCghC9IOr1pvJlVXxo8V7n0TwOb9BNXQeZAJic700gj3gXiAtgIkddm38OTutnPNtNfjV2OU6u4mqSzBiSWLyXp3Qv4BfHbV2x2Xud6yIIeyQZDZD'
 Facebook::Messenger.config.app_secret = '__app_secret_here__'
 Facebook::Messenger.config.verify_token = 'xIn9f1mYAi220VU729805f7o45P5Ad16ENV'
 Facebook::Messenger::Subscriptions.subscribe
